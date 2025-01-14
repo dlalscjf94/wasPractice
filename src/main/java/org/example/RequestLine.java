8fe133e81b7c58cd01b9f94af7cd807e5fcd842f
dlalscjf94 <ssw01092@naver.com>
2025-01-15 01:58:58 +0900

    -feat : method, urlPath, queryString 생성자 추가 및 equals  테스트 추가

--- src/main/java/org/example/RequestLine.java
+++ src/main/java/org/example/RequestLine.java
@@ -3,15 +3,49 @@ package org.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Objects;
+
 public class RequestLine {
 
     private final String method; //GET
+    private final String urlPath;
+    private String queryString;
 
     private  static  final Logger logger = LoggerFactory.getLogger(RequestLine.class);
-    public RequestLine(String requestLine) {
 
+    public RequestLine(String method, String urlPath, String queryString) {
+        this.method = method;
+        this.urlPath = urlPath;
+        this.queryString = queryString;
+    }
+
+    public RequestLine(String requestLine) {
+        // GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
         String[] tokens = requestLine.split(" ");
         this.method = tokens[0];
         logger.info("method check :: {}", method);
+        for (int i = 0; i < tokens.length; i++) {
+            logger.debug("splited token check :: {}", tokens[i]);
+        }
+        // 1 위치 /calculate?operand1=11&operator=*&operand2=55
+        String[] urlPathTokens = tokens[1].split("\\?");
+        this.urlPath = urlPathTokens[0];
+
+        if (urlPathTokens.length == 2) {
+            this.queryString = urlPathTokens[0];
+        }
+    }
+    // 객체끼리 비교할 때 equals and hashCode 필요
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RequestLine that = (RequestLine) o;
+        return Objects.equals(method, that.method) && Objects.equals(urlPath, that.urlPath) && Objects.equals(queryString, that.queryString);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(method, urlPath, queryString);
     }
 }
